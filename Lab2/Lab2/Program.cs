1d8769eea612f8daff92e69ce7b46ed3b82c94db
I <ilya4reg@mail.ru>
2018-09-16 23:50:04 +0700

    C4

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -15,7 +15,7 @@ namespace Lab2
             Console.WriteLine("Hello World!!");
             Console.ReadKey();
             //Comment11 
-
+            //Comment for L2
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
